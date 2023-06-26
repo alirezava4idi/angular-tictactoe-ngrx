6705bbcedfedfb5c6863872bba7278e459f86f34
alirezava4idi <alireza.vahidi1@gmail.com>
2023-06-26 14:42:50 +0330

    made the app an PWA

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { AppComponent } from './app.component';
 
 import { gameReducer } from '../reducers/game.reducer';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
   declarations: [
@@ -15,6 +16,12 @@ import { gameReducer } from '../reducers/game.reducer';
     StoreModule.forRoot({game: gameReducer}),
     StoreDevtoolsModule.instrument({
       maxAge: 10
+    }),
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
     })
   ],
   providers: [],
